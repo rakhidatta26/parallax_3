bd00a9e10ab8d07e132a252168d4b0e204ef5968
Rakhi Datta <146545970+rakhidatta26@users.noreply.github.com>
2023-12-26 19:54:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	 
+ $('#parallax').xsParallax({
+  bgFolder:'xspl-backgrounds',
+fileExtension:'.jpg',
+  speed: 5,
+   offset: 100,
+direction:'vertical'
+});    
+});
+
